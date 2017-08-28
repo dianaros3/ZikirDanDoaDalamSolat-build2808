8052bcf2d30ced1ec8e88b03b057225212bb8fb4
dianaros3 <nuruldiana91@gmail.com>
2017-08-28 09:39:08 +0800

    inity

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,209 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Back */
+    
+    
+        /* button  .uib_w_1 */
+    $(document).on("click", ".uib_w_1", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#info"); 
+         return false;
+    });
+    
+        /* button  .uib_w_81 */
+    $(document).on("click", ".uib_w_81", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_5 */
+    $(document).on("click", ".uib_w_5", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#IFTITAH"); 
+         return false;
+    });
+    
+        /* button  .uib_w_7 */
+    $(document).on("click", ".uib_w_7", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ISTIADZAH"); 
+         return false;
+    });
+    
+        /* button  .uib_w_30 */
+    $(document).on("click", ".uib_w_30", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  GO */
+    $(document).on("click", ".uib_w_82", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_6 */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#RUKU"); 
+         return false;
+    });
+    
+        /* button  .uib_w_122 */
+    $(document).on("click", ".uib_w_122", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_240 */
+    $(document).on("click", ".uib_w_240", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_301 */
+    $(document).on("click", ".uib_w_301", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_277 */
+    $(document).on("click", ".uib_w_277", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_190 */
+    $(document).on("click", ".uib_w_190", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_171 */
+    $(document).on("click", ".uib_w_171", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  .uib_w_8 */
+    $(document).on("click", ".uib_w_8", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ITIDAL"); 
+         return false;
+    });
+    
+        /* button  .uib_w_9 */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#SUJUD"); 
+         return false;
+    });
+    
+        /* button  .uib_w_10 */
+    $(document).on("click", ".uib_w_10", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#DUDUKANTARADUASUJUD"); 
+         return false;
+    });
+    
+        /* button  .uib_w_11 */
+    $(document).on("click", ".uib_w_11", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#TASYAHUD"); 
+         return false;
+    });
+    
+        /* button  .uib_w_12 */
+    $(document).on("click", ".uib_w_12", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#SELAWAT"); 
+         return false;
+    });
+    
+        /* button  .uib_w_13 */
+    $(document).on("click", ".uib_w_13", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#SALAM"); 
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_327", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_328", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ISTIFTAH"); 
+         return false;
+    });
+    
+        /* button  .uib_w_257 */
+    $(document).on("click", ".uib_w_257", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_344", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#test"); 
+         return false;
+    });
+    
+        /* button  Let's Start */
+    $(document).on("click", ".uib_w_346", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Content"); 
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
